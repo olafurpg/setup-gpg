eac449c0239936c3c6c2730895e9c41911915f7d
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-09-23 11:13:57 +0100

    Implement basic action to install gpg v1

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as shelljs from "shelljs";
+import * as path from "path";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    core.startGroup("Download gpg");
+    shelljs.exec(
+      "wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2"
+    );
+    shelljs.exec("bzip2 -d gnupg-1.4.23.tar.bz2");
+    shelljs.exec("tar xvf gnupg-1.4.23.tar");
+    shelljs.cd("gnupg-1.4.23");
+    core.endGroup();
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    core.startGroup("./configure");
+    shelljs.exec("./configure");
+    core.endGroup();
 
-    core.setOutput('time', new Date().toTimeString());
+    core.startGroup("make");
+    shelljs.exec("make");
+    const g10 = path.join(shelljs.pwd(), "g10");
+    core.addPath(g10);
+    core.endGroup();
+
+    shelljs.exec(`./${path.join(g10, "gpg")} --version`);
   } catch (error) {
     core.setFailed(error.message);
   }
