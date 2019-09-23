95123825eec0608d1f46c15e0f735961256c69c8
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-09-23 11:22:49 +0100

    Run basic test

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,31 @@ import * as path from "path";
 
 async function run() {
   try {
-    core.startGroup("Download gpg");
-    shelljs.exec(
-      "wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2"
-    );
-    shelljs.exec("bzip2 -d gnupg-1.4.23.tar.bz2");
-    shelljs.exec("tar xvf gnupg-1.4.23.tar");
-    shelljs.cd("gnupg-1.4.23");
-    core.endGroup();
-
-    core.startGroup("./configure");
-    shelljs.exec("./configure");
-    core.endGroup();
-
-    core.startGroup("make");
-    shelljs.exec("make");
-    const g10 = path.join(shelljs.pwd(), "g10");
-    core.addPath(g10);
-    core.endGroup();
-
-    shelljs.exec(`./${path.join(g10, "gpg")} --version`);
+    installGpg();
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+export function installGpg() {
+  core.startGroup("Download gpg");
+  shelljs.exec("wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2");
+  shelljs.exec("bzip2 -d gnupg-1.4.23.tar.bz2");
+  shelljs.exec("tar xvf gnupg-1.4.23.tar");
+  shelljs.cd("gnupg-1.4.23");
+  core.endGroup();
+
+  core.startGroup("./configure");
+  shelljs.exec("./configure");
+  core.endGroup();
+
+  core.startGroup("make");
+  shelljs.exec("make");
+  const g10 = path.join(shelljs.pwd(), "g10");
+  core.addPath(g10);
+  core.endGroup();
+
+  shelljs.exec(`./${path.join(g10, "gpg")} --version`);
 }
 
 run();
